66d055072751da61cab24895a70502964bc52351
Marco Valle <marco_97@live.com.mx>
2023-06-05 15:55:13 -0600

    Configuración de mf multirepo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'https://storage.cloud.google.com/mf-autos/remoteEntry.js',
+        exposedModule: './ProductsModule',
+      }).then((m) => m.ProductsModule),
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
